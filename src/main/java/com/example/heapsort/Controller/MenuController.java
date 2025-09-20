eed96970ca4f24634867d3f623fea3257ad5e437
Maria Isabelly Andrade <andradeisabellypessoal@gmail.com>
2025-09-20 01:09:51 -0300

    tim corrigido

--- src/main/java/com/example/heapsort/Controller/MenuController.java
+++ src/main/java/com/example/heapsort/Controller/MenuController.java
@@ -2,46 +2,39 @@ package com.example.heapsort.Controller;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 import javafx.scene.control.Button;
-
-import java.awt.event.ActionEvent;
+import javafx.stage.Stage;
 import java.io.IOException;
 
 public class MenuController {
 
     @FXML
-    private Button btHeap, btTim;
+    private Button btHeap;
 
     @FXML
+    private Button btTim;
 
+    @FXML
     private void abrirHeapSort() throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/heapsort/hello-view.fxml"));
         Parent root = loader.load();
 
         Stage stage = new Stage();
+        stage.setTitle("Animação de Ordenação - Heap Sort");
         stage.setScene(new Scene(root));
         stage.show();
     }
 
     @FXML
-    private void abrirTimSort()  throws IOException  {
-
+    private void abrirTimSort() throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/heapsort/tim.fxml"));
         Parent root = loader.load();
 
         Stage stage = new Stage();
+        stage.setTitle("Animação de Ordenação - Tim Sort");
         stage.setScene(new Scene(root));
         stage.show();
     }
-
-
-
-
-
-
 }
